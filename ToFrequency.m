e49db3f49b398f3fb0722bcd4368fb45b49d5270
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-09-12 15:59:03 +1000

    For old Matlab version

--- ToFrequency.m
+++ ToFrequency.m
@@ -1,4 +1,4 @@
-function ToFrequency(x,fs,doPlot)
+function ToFrequency(x,fs,doPlot,oldMatlab)
 
 % Set default sampling rate:
 if nargin < 2
@@ -8,13 +8,25 @@ end
 if nargin < 3
     doPlot = true;
 end
+if nargin < 4
+    oldMatlab = false;
+end
 %-------------------------------------------------------------------------------
 
 N = length(x);
 t = (1:1/fs:N/fs)';
-xTable = timetable(seconds(t),zscore(x));
 
-[pxx,f] = pspectrum(xTable);
+if oldMatlab
+    Y = fft(x);
+    P2 = abs(Y/N);
+    P1 = P2(1:N/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    pxx = P1.^2;
+    f = fs*(0:(N/2))/N;
+else
+    xTable = timetable(seconds(t),zscore(x));
+    [pxx,f] = pspectrum(xTable);
+end
 
 if doPlot
     plot(f,pow2db(pxx),'k')
